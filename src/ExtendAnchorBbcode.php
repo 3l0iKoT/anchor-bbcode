741b11db7c520a4cb85f6645320b6f8598de5218
3l0i_Kot <arsenghj@mail.ru>
2025-02-08 23:26:49 +0500

    a

---
+++ src/ExtendAnchorBbcode.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace YourName\AnchorBbcode;
+
+use Flarum\Extend;
+use s9e\TextFormatter\Configurator;
+
+return [
+    (new Extend\Formatter)
+        ->configure(function (Configurator $config) {
+            // Добавляем кастомный BBCode тег для якоря
+            $config->BBCodes->addCustom(
+                '[anchor={TEXT}]',
+                '<span id="{TEXT}"></span>',
+                ['parseContent' => false]
+            );
+
+            // Добавляем кастомный BBCode тег для ссылки на якорь
+            $config->BBCodes->addCustom(
+                '[link={TEXT1}]{TEXT2}[/link]',
+                '<a href="#{TEXT1}">{TEXT2}</a>',
+                ['parseContent' => false]
+            );
+        })
+];
